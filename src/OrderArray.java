157ff548997a22125ce54696a2eddacfe8463c15
Bayron Cárdenas <bcardenas@endava.com>
2018-08-03 17:25:24 -0500

    quick ordering method  (quicksort) was created and test were adapted for this. The result of all test was successful

--- src/OrderArray.java
+++ src/OrderArray.java
@@ -30,4 +30,37 @@ public class OrderArray {
             return array;
         }
     }
+
+    private static int Partition(int[] array, int indexLow, int indexHigh) {
+        int pivot = array[indexHigh];
+        int i = indexLow - 1;
+        int acu;
+        for (int j = indexLow; j <= indexHigh - 1; j++) {
+            if (array[j] < pivot) {
+                i = i + 1;
+                acu = array[i];
+                array[i] = array[j];
+                array[j] = acu;
+            }
+        }
+        acu = array[i + 1];
+        array[i + 1] = array[indexHigh];
+        array[indexHigh] = acu;
+        return i + 1;
+    }
+
+    public int[] QuickSort(int[] array, int indexLow, int indexHigh) {
+        if (array == null) {
+            return null;
+        } else {
+            if (array.length != 0) {
+                if (indexLow < indexHigh) {
+                    int p = Partition(array, indexLow, indexHigh);
+                    QuickSort(array, indexLow, p - 1);
+                    QuickSort(array, p + 1, indexHigh);
+                }
+            }
+            return array;
+        }
+    }
 }
--- src/Test.java
+++ src/Test.java
@@ -1,10 +1,15 @@
 public class Test {
 
-    public static boolean OrderTest() {
+    public static boolean OrderTest(int opc) {
         int[] testArray = {20, 1, 5, 14, 34, 7, 9, 17, 23, 10};
         int[] expectedResult = {1, 5, 7, 9, 10, 14, 17, 20, 23, 34};
         OrderArray sortArray = new OrderArray();
-        int[] result = sortArray.Order(testArray);
+        int[] result;
+        if (opc == 1) {
+            result = sortArray.Order(testArray);
+        } else {
+            result = sortArray.QuickSort(testArray, 0, testArray.length-1);
+        }
         int cont = 0;
         for (int i = 0; i < result.length; i++) {
             if (result[i] == expectedResult[i]) {
@@ -14,28 +19,38 @@ public class Test {
         return cont == expectedResult.length;
     }
 
-    public static boolean TestForEmptyArray() {
+    public static boolean TestForEmptyArray(int opc) {
         OrderArray sortArray = new OrderArray();
-        int[] result = sortArray.Order(new int[0]);
+        int[] result;
+        if (opc == 1) {
+            result = sortArray.Order(new int[0]);
+        } else {
+            result = sortArray.QuickSort(new int[0], 0, 0);
+        }
         return result.length == 0;
     }
 
-    public static boolean TestForNullArray() {
+    public static boolean TestForNullArray(int opc) {
         OrderArray sortArray = new OrderArray();
-        int[] result = sortArray.Order(null);
+        int[] result;
+        if (opc == 1) {
+            result = sortArray.Order(null);
+        } else {
+            result = sortArray.QuickSort(null, 0, 0);
+        }
         return result == null ? true : false;
     }
 
     public static void main(String[] args) {
 
-        if (OrderTest()) {
+       if (OrderTest(2)) {
             System.out.println("Ordering Test with array not null and not empty was successful");
         }
-        if (TestForEmptyArray()) {
+        if (TestForEmptyArray(2)) {
             System.out.println("Ordering Test for empty array was successful");
         }
 
-        if (TestForNullArray()) {
+        if (TestForNullArray(2)) {
             System.out.println("Ordering test for null array was successful");
         }
     }
