a00314508f776d5353430f25c4e290b5d0163951
Bayron Cárdenas <bcardenas@endava.com>
2018-08-03 15:13:58 -0500

    Basic ordering methos was created and ordering test for not null and not empty array was successful

--- src/OrderArray.java
+++ src/OrderArray.java
@@ -1,7 +1,27 @@
-public class OrderArray
-{
-    public OrderArray()
-    {
+public class OrderArray {
 
+    public OrderArray() {
+
+    }
+
+    public int[] Order(int[] array) {
+        boolean ordered = false;
+        int acu;
+        int cont = 0;
+        while (!ordered) {
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i + 1] < array[i]) {
+                    acu = array[i];
+                    array[i] = array[i + 1];
+                    array[i+1] = acu;
+                } else {
+                    cont++;
+                }
+            }
+            if (cont == array.length) {
+                ordered = true;
+            }
+        }
+        return array;
     }
 }
--- src/Test.java
+++ src/Test.java
@@ -1,14 +1,23 @@
 public class Test {
 
     public static boolean OrderTest() {
-        double[] testArray = {20, 1, 5, 14, 34, 7, 9, 17, 23, 10};
-        double[] expectedResult = {1, 5, 7, 9, 10, 14, 17, 20, 23, 34};
+        int[] testArray = {20, 1, 5, 14, 34, 7, 9, 17, 23, 10};
+        int[] expectedResult = {1, 5, 7, 9, 10, 14, 17, 20, 23, 34};
         OrderArray sortArray = new OrderArray();
-        double[] result = sortArray.Order(testArray);
-        return result == expectedResult ? true : false;
+        int[] result = sortArray.Order(testArray);
+        int cont=0;
+        for (int i = 0; i < result.length; i++) {
+            if (result[i] == expectedResult[i]) {
+                cont++;
+            }
+        }
+        return cont == expectedResult.length ? true : false;
     }
 
     public static void main(String[] args) {
-        OrderTest();
+
+        if (OrderTest()) {
+            System.out.println("Ordering Test with array not null and not empty was successful");
+        }
     }
 }
