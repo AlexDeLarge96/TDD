aeabbab3c07a41e019113721cb2ac788d2c69621
Bayron Cárdenas <bcardenas@endava.com>
2018-08-03 15:46:06 -0500

    changes on order method were added and test for null array was successful

--- src/OrderArray.java
+++ src/OrderArray.java
@@ -8,22 +8,26 @@ public class OrderArray {
         boolean ordered = false;
         int acu;
         int cont = 0;
-        if (array.length != 0) {
-            while (!ordered) {
-                for (int i = 0; i < array.length - 1; i++) {
-                    if (array[i + 1] < array[i]) {
-                        acu = array[i];
-                        array[i] = array[i + 1];
-                        array[i + 1] = acu;
-                    } else {
-                        cont++;
+        if (array == null) {
+            return null;
+        } else {
+            if (array.length != 0) {
+                while (!ordered) {
+                    for (int i = 0; i < array.length - 1; i++) {
+                        if (array[i + 1] < array[i]) {
+                            acu = array[i];
+                            array[i] = array[i + 1];
+                            array[i + 1] = acu;
+                        } else {
+                            cont++;
+                        }
+                    }
+                    if (cont == array.length) {
+                        ordered = true;
                     }
                 }
-                if (cont == array.length) {
-                    ordered = true;
-                }
             }
+            return array;
         }
-        return array;
     }
 }
