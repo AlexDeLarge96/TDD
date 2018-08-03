082709e34d74d0e1aef88249d3a8f5387af2c203
Bayron Cárdenas <bcardenas@endava.com>
2018-08-03 15:39:03 -0500

    Test for empty array was successful

--- src/Test.java
+++ src/Test.java
@@ -1,12 +1,11 @@
 public class Test {
 
-    private OrderArray sortArray = new OrderArray();
-
     public static boolean OrderTest() {
         int[] testArray = {20, 1, 5, 14, 34, 7, 9, 17, 23, 10};
         int[] expectedResult = {1, 5, 7, 9, 10, 14, 17, 20, 23, 34};
+        OrderArray sortArray = new OrderArray();
         int[] result = sortArray.Order(testArray);
-        int cont=0;
+        int cont = 0;
         for (int i = 0; i < result.length; i++) {
             if (result[i] == expectedResult[i]) {
                 cont++;
@@ -15,16 +14,19 @@ public class Test {
         return cont == expectedResult.length;
     }
 
-    public static boolean TestForEmptyArray()
-    {
-        int[] result=sortArray.Order(new int[5]);
-        return result.length==0;
+    public static boolean TestForEmptyArray() {
+        OrderArray sortArray = new OrderArray();
+        int[] result = sortArray.Order(new int[0]);
+        return result.length == 0;
     }
 
     public static void main(String[] args) {
 
         if (OrderTest()) {
             System.out.println("Ordering Test with array not null and not empty was successful");
+        }
+        if (TestForEmptyArray()) {
+            System.out.println("Ordering Test fro empty array was successful");
         }
     }
 }
